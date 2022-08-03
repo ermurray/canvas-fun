375011c24610ef7bdbb3bf1013053a8655864d6c
ermurray <eric@ermurray.ca>
2022-08-03 17:31:18 -0600

    WIP fractal tree on canvas

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -6,26 +6,28 @@ canvas.height = window.innerHeight;
   console.log(ctx);
 
 
-function drawfractal(startX, startY, length, angle, segmentW, color1, color2){
+function drawTree(startX, startY, length, angle, segmentW, color1, color2){
   ctx.beginPath();
   ctx.save();
   ctx.strokeStyle = color1;
   ctx.fillStyle = color2;
   ctx.lineWidth = segmentW;
+  ctx.lineCap = "round";
   ctx.translate(startX, startY)
   ctx.rotate(angle * Math.PI/180);
   ctx.moveTo(0,0);
   ctx.lineTo(0, -length);
   ctx.stroke();
+ 
 
-  if (length < 5) {
+  if (length < 5 || segmentW < 1) {
     ctx.restore();
     return;
   }
-  drawfractal(0, -length * 0.95, length * 0.75, angle + 15, segmentW * 0.75);
-  drawfractal(0, -length * 0.95, length * 0.75, angle - 15, segmentW * 0.75);
+  drawTree(0, -length * 0.95, length * 0.75, angle + 15, segmentW * 0.75);
+  drawTree(0, -length * 0.95, length * 0.75, angle - 15, segmentW * 0.75);
 
   ctx.restore();
 }
 
-drawfractal(canvas.width/2, canvas.height -40, 120, 0, 10, 'brown', 'green')
+drawTree(canvas.width/2, canvas.height -40, 200, 0, 20, 'brown', 'green')

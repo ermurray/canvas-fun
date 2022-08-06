86f884e33c61fd2b3d8b652a4f755e7a6e59562b
ermurray <eric@ermurray.ca>
2022-08-06 01:20:48 -0600

    hide or show canvas on button click

---
+++ public/scripts/fractal-trees.js
@@ -0,0 +1,33 @@
+const canvas = document.getElementById('fractal-trees');
+const ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight - 60;
+
+  console.log(ctx);
+
+
+function drawTree(startX, startY, length, angle, segmentW, color1, color2){
+  ctx.beginPath();
+  ctx.save();
+  ctx.strokeStyle = color1;
+  ctx.fillStyle = color2;
+  ctx.lineWidth = segmentW;
+  ctx.lineCap = "round";
+  ctx.translate(startX, startY)
+  ctx.rotate(angle * Math.PI/180);
+  ctx.moveTo(0,0);
+  ctx.lineTo(0, -length);
+  ctx.stroke();
+ 
+
+  if (length < 5 || segmentW < 1) {
+    ctx.restore();
+    return;
+  }
+  drawTree(0, -length * 0.95, length * 0.75, angle + 15, segmentW * 0.75);
+  drawTree(0, -length * 0.95, length * 0.75, angle - 15, segmentW * 0.75);
+
+  ctx.restore();
+}
+
+drawTree(canvas.width/2, canvas.height -40, 185, 0, 20, 'brown', 'green')
--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,33 +1,15 @@
-const canvas = document.getElementById('fractal-trees');
-const ctx = canvas.getContext('2d');
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight - 60;
 
-  console.log(ctx);
+  const myTreesButton = document.getElementById('getMyTrees');
+  const newtonsApple = document.getElementById('apple');
+  const treeCanvas = document.getElementById('fractal-trees');
+  const fallingApples = document.getElementById('newtonian-physics')
+  
+  myTreesButton.addEventListener('click', () => {
+    treeCanvas.style.display = 'block';
+    fallingApples.style.display = 'none';
+  });
 
-
-function drawTree(startX, startY, length, angle, segmentW, color1, color2){
-  ctx.beginPath();
-  ctx.save();
-  ctx.strokeStyle = color1;
-  ctx.fillStyle = color2;
-  ctx.lineWidth = segmentW;
-  ctx.lineCap = "round";
-  ctx.translate(startX, startY)
-  ctx.rotate(angle * Math.PI/180);
-  ctx.moveTo(0,0);
-  ctx.lineTo(0, -length);
-  ctx.stroke();
- 
-
-  if (length < 5 || segmentW < 1) {
-    ctx.restore();
-    return;
-  }
-  drawTree(0, -length * 0.95, length * 0.75, angle + 15, segmentW * 0.75);
-  drawTree(0, -length * 0.95, length * 0.75, angle - 15, segmentW * 0.75);
-
-  ctx.restore();
-}
-
-drawTree(canvas.width/2, canvas.height -40, 200, 0, 20, 'brown', 'green')
+  newtonsApple.addEventListener('click', () => {
+    treeCanvas.style.display = 'none';
+    fallingApples.style.display = 'block';
+  })
